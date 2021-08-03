80889bc7d60b8357f2a8222a2e76845da67bbf39
cristian sepulveda <cristiangsc@gmail.com>
2021-08-03 17:41:15 -0400

    modelos finish

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie_id, uniqueness: {scope: :list_id, messages: "ya existe en la lista el mismo comentario"}
 end
